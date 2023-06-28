3569f613c0198874c7390e74e5bdf3143551d9e4
samielhembromsammi <hembroms965@gmail.com>
2023-06-28 20:54:55 +0600

    routing
    rounting page

--- pactis01/index.js
+++ pactis01/index.js
@@ -5,6 +5,18 @@ app.get("/",function(res,res){
     res.send("hello Express js sammi hembrom samiel ")
 });
 
+app.get("/about",function(res,res){
+    res.send("hello Express js about page sammi hembrom samiel ")
+});
+app.get("/contact",function(res,res){
+    res.send("hello Express js  contact sammi hembrom samiel ")
+});
+app.get("/terems",function(res,res){
+    res.send("hello Express js terems page sammi hembrom samiel ")
+});
+app.get("/outsite",function(res,res){
+    res.send("hello Express js outsite page sammi hembrom samiel ")
+});
 app.listen(8080,function(){
     console.log("server run Success")
 });
